bac4bd013ebb8f45399cddd82f8b4ba9fb021374
Rigan Asumani <rigan.gachoka@student.moringaschool.com>
2021-11-29 12:44:43 +0300

    new

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 alert("WELCOME TO RIGANS PIZZA");
+$("span#crust-price").text(inputCrust.toFixed(2));
+$("span#size-price").text(inputSize.toFixed(2));
+$("span#toppings-price").text(inputToppings.toFixed(2));
+$("span#total").text(price);
