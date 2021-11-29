5ca24b8b742eb7e1faa5fd4b1bf7ba7b0c7125f5
Rigan Asumani <rigan.gachoka@student.moringaschool.com>
2021-11-29 12:56:22 +0300

    add

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ $("span#crust-price").text(inputCrust.toFixed(2));
 $("span#size-price").text(inputSize.toFixed(2));
 $("span#toppings-price").text(inputToppings.toFixed(2));
 $("span#total").text(price);
+  
+               alert (" Hello \n Feel free to checkout our latest offers ");  
+               
